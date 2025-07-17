69998b22a448f74dcb1be3d3140b0cbb0941f5ba
Eyre Xi <109657355+Eyre-dutchess@users.noreply.github.com>
2025-07-17 09:00:38 +0800

    Update ChessPage.tsx

--- src/pages/ChessPage.tsx
+++ src/pages/ChessPage.tsx
@@ -4,7 +4,7 @@ import { HiOutlineArrowLeftOnRectangle } from "react-icons/hi2"
 import { Link } from "react-router-dom"
 
 
-export const HangManPage = () => {
+export const ChessPage = () => {
   return(
     <Container>
       <Link to="/" className="absolute top-5 left-5 border border-white/50 text-white rounded-full p-2 cursor-pointer hover:bg-white hover:text-blue-400">
